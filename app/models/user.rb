6b74d5ed7e637c273e7b6eaefbc061ce3ce3e77a
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-08-18 17:42:53 +0900

    validation設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   has_many :comments
 
   validates :nickname, presence: true, length: { maximum: 6 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
